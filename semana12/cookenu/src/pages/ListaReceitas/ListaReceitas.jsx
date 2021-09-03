d835a45a8d3188f6eeb3293fc619e176396ad17b
natanbenfica <natanbenficabarbosa@gmail.com>
2021-09-02 21:42:01 -0300

    implementação da sta de receitasas

--- semana12/cookenu/src/pages/ListaReceitas/ListaReceitas.jsx
+++ semana12/cookenu/src/pages/ListaReceitas/ListaReceitas.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import { useProtectedPage } from "../../hooks/useProtectedPage";
+import { RecipeCard } from "../../components/RecipeCard/RecipeCard";
+import { useRequestData } from "../../hooks/useRequestData";
+import { BaseUrl } from "../../constants/Urls";
+import { RecipeListContainer } from "./styled";
+import { AddRecipeButton } from "./styled";
+import { Add } from "@material-ui/icons";
+import { irParaAdicionarReceita } from "../../router/Coordinator";
+import { useHistory } from "react-router";
 
 export const ListaReceitas = () => { // import com chaves no router
-    
     useProtectedPage()
+    const recipes = useRequestData([], `${BaseUrl}/recipe/feed`)
+    const history = useHistory()
+
+    const onClickCard = () => {
+        console.log("teste")
+    }
+
+    const recipesCards = recipes.map((recipe) => {
+        return (
+            <RecipeCard
+                key={recipe.recipe_id}
+                title={recipe.title}
+                image={recipe.image}
+                onClick={onClickCard()}
+            />
+        )
+    })
 
     return (
-        <div>
-            <h1>Lista de Receitas</h1>
-        </div>
+        <RecipeListContainer>
+            {recipesCards}
+            <AddRecipeButton
+                color={"primary"}
+            >
+                <Add onClick={() => irParaAdicionarReceita(history)} />
+            </AddRecipeButton>
+        </RecipeListContainer>
     )
 }
