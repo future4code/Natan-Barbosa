d28ae7bca26c6a95c59a37c1e176b1ef21f36645
natanbenfica <natanbenficabarbosa@gmail.com>
2021-09-26 14:04:44 -0300

    colocando o servidor no ar com express e cors

--- semana15/sistema-bancario/src/index.ts
+++ semana15/sistema-bancario/src/index.ts
@@ -1,1 +1,12 @@
-console.log(`Hello, Back-end!`)
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log(`O servidor TÁ ON na porta 3003`)
+})
+
