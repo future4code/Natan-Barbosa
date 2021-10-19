d4b448a04e061860729fcdbf0d7105015a26c935
natanbenfica <natanbenficabarbosa@gmail.com>
2021-10-18 21:16:10 -0300

    Introdução à Autenticação

---
+++ semana19/intro-auth/src/index.ts
@@ -0,0 +1,6 @@
+import app from "./app"
+import editUser from './endpoints/editUser'
+import createUser from './endpoints/createUser'
+
+app.post('/users/signup', createUser)
+app.put('/users/:id', editUser)
