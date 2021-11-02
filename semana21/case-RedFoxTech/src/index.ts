26cd181662be9cb087cb036d8f5162de91050152
natanbenfica <natanbenficabarbosa@gmail.com>
2021-11-01 22:34:40 -0300

    configuracoes iniciais do projeto

---
+++ semana21/case-RedFoxTech/src/index.ts
@@ -0,0 +1,31 @@
+import knex from 'knex'
+import dotenv from 'dotenv'
+import { AddressInfo } from "net";
+
+dotenv.config()
+
+const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE_NAME,
+    }
+})
+
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
