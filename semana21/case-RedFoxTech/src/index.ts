b07132e450246109ac89938e5f63cf63f59a07fc
natanbenfica <natanbenficabarbosa@gmail.com>
2021-11-02 21:17:56 -0300

    dependencias e conexao com o bd

--- semana21/case-RedFoxTech/src/index.ts
+++ semana21/case-RedFoxTech/src/index.ts
@@ -1,6 +1,15 @@
+import express, { Express } from 'express'
+import cors from 'cors'
 import knex from 'knex'
 import dotenv from 'dotenv'
 import { AddressInfo } from "net";
+import { v4 } from "uuid";
+import * as bcrypt from "bcryptjs";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json())
 
 dotenv.config()
 
@@ -15,11 +24,32 @@ const connection = knex({
     }
 })
 
-import express from 'express'
+class IdGenerator {
 
-const app = express()
+    generateId(): string {
+        return v4();
+    }
+}
 
-app.use(express.json())
+class hashManager {
+
+    async hash(s: string): Promise<string> {
+        const cost = Number(process.env.BCRYPT_COST!);
+        const salt = await bcrypt.genSalt(cost);
+        const cypherText = await bcrypt.hash(s, salt);
+
+        return cypherText;
+    }
+
+    async compare(plaintext: string, hash: string): Promise<boolean> {
+        const hashCompare: boolean = await bcrypt.compare(plaintext, hash);
+        return hashCompare;
+    }
+}
+
+
+
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
