d47828181661c910b8cccee5114c131b9cf0f233
natanbenfica <natanbenficabarbosa@gmail.com>
2021-11-22 21:47:52 -0300

    setup do projeto

---
+++ semana24/case-brainn/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MegaSenaPage } from "./Pages/MegaSenaPage/MegaSenaPage";
+
+const App = () => {
+  return (
+    <div>
+      <MegaSenaPage></MegaSenaPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ semana24/case-brainn/src/Pages/MegaSenaPage/MegaSenaPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MegaSenaPage = () => {
+    return (
+        <div>
+            <svg height="660" viewBox="0 0 613 1080" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M613 0C613 0 361.26 501.011 613 1080H0V0H613Z" fill="#6BEFA3" />
+            </svg>
+        </div>
+    )
+}
